38cca98e9e59ff81e13e1eda3016c50313bf72bf
Mansky446 <mansky446@gmail.com>
2024-11-28 03:25:06 +0700

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,100 +1,55 @@
-import re
+from re import search
+from os import system
 
-list_lagu = []
+import modul.data_lagu as data_lagu
 
-def dapatkan_index_lagu(judul_lagu):
-    jumlah_lagu = len(list_lagu)
+def dapatkan_input_dan_cek(pesan, pesan_salah, regex):
+    while True:
+        print(pesan)
+        string_input = input("> ").strip()
 
-    if jumlah_lagu != 0:
-        for i in range(jumlah_lagu):
-            lagu = list_lagu[i]
+        if search(regex, string_input) is not None:
+            return string_input
+        
+        print(pesan_salah)
 
-            if lagu["judul"] == judul_lagu:
-                return i
+def tampilkan_menu():
+    print("Selamat Datang di Manajemen Lagu")
+    print("---------------------------------")
+    print("1. Tambah lagu")
+    print("2. Hapus lagu")
+    print("3. Edit lagu")
+    print("4. Lihat lagu")
+    print("5. Lihat detail lagu")
+    print("6. Keluar")
+    print("")
+
+    opsi = input("Pilih opsi (1-6): ")
+    print("")
+
+    try:
+        opsi = int(opsi)
     
-    return -1
-
-
-def tambah_lagu(judul, penyanyi, album, genre, tahun, durasi):
-    if dapatkan_index_lagu(judul) != -1:
-        return False
+    except:
+        print("Input opsi tidak sesuai!")
+        print("")
     
-    list_lagu.append({
-        "judul": judul,
-        "penyanyi": penyanyi,
-        "album": album,
-        "genre": genre,
-        "tahun": tahun,
-        "durasi": durasi
-    })
-
-    return True
-
-def edit_lagu(lagu, judul, penyanyi, album, genre, tahun, durasi):
-    data_edit = []
-
-    def edit(tipe, data_baru):
-        data_edit.append({ "tipe": tipe, "data_sebelumnya": lagu[tipe], "data_baru" : data_baru})
-        lagu["judul"] = data_baru
-
-    if len(judul) != 0: edit("judul", judul)
-    if len(penyanyi) != 0: edit("penyanyi", penyanyi)
-    if len(album) != 0: edit("album", album)
-    if len(genre) != 0: edit("genre", genre)
-    if len(tahun) != 0: edit("tahun", tahun)
-    if len(durasi) != 0: edit("durasi", durasi)
-
-    return None if len(data_edit) == 0 else data_edit
+    else:
+        if opsi < 1 or opsi > 6:
+            print("Masukkan opsi 1-6!")
+            print("")
     
-def hapus_lagu(judul):
-    index_lagu = dapatkan_index_lagu(judul)
+    return opsi
 
-    if index_lagu == -1:
-        return False
-    
-    list_lagu.pop(index_lagu)
-
-    return True
 
 if __name__ == "__main__":
+    data_lagu.inisiasi()
+
     while True:
-        print("")
-        print("Selamat Datang di Manajemen Lagu")
-        print("---------------------------------")
-        print("1. Tambah lagu")
-        print("2. Hapus lagu")
-        print("3. Edit lagu")
-        print("4. Lihat lagu")
-        print("5. Lihat detail lagu")
-        print("6. Keluar")
-        print("")
-
-        opsi = input("Pilih opsi (1-6): ")
-        print("")
-
-        try:
-            opsi = int(opsi)
-        except:
-            print("Input opsi tidak sesuai!")
-            print("")
-            continue
-        else:
-            if opsi < 1 and opsi > 6:
-                print("Masukkan opsi 1-6!")
-                print("")
-                continue
+        system("cls")
+        opsi = tampilkan_menu()
         
         if opsi == 1:
-            def dapatkan_input_dan_cek(pesan, pesan_salah, regex):
-                while True:
-                    print(pesan)
-                    string_input = input("> ").strip()
-
-                    if re.search(regex, string_input) is not None:
-                        return string_input
-                    
-                    print(pesan_salah)
-
             judul = dapatkan_input_dan_cek("Masukkan judul lagu", "Input judul tidak sesuai, harap diulang!", r"^\w[\w\s']*$")
             album = dapatkan_input_dan_cek("Masukkan Album lagu", "Input album tidak sesuai, harap diulang!", r"^\w[\w\s']*$")
             penyanyi = dapatkan_input_dan_cek("Masukkan penyayi lagu", "Input penyanyi tidak sesuai, harap diulang!", r"^\w[\w\s']*$")
@@ -103,16 +58,16 @@ if __name__ == "__main__":
             durasi = dapatkan_input_dan_cek("Masukkan durasi lagu [mm:dd]", "Input durasi tidak sesuai, harap diulang!", r"^[0-9]{2}:[0-9]{2}$")
             print("")
 
-            if tambah_lagu(judul, penyanyi, album, genre, tahun, durasi):
-                print("Lagu %s berhasil ditambahkan" % (judul))
+            if data_lagu.tambah_lagu(judul, penyanyi, album, genre, tahun, durasi):
+                print(f"Lagu {judul} berhasil ditambahkan")
                 print("")
 
             else:
-                print("Lagu %s sudah ada" % (judul))
+                print(f"Lagu {judul} sudah ada")
                 print("")
             
         elif opsi == 2:
-            if len(list_lagu) == 0:
+            if data_lagu.apakah_lagu_kosong():
                 print("Belum ada lagu untuk saat ini")
                 print("")
             
@@ -121,16 +76,16 @@ if __name__ == "__main__":
                 judul = input("> ").strip()
                 print("")
 
-                if hapus_lagu(judul):
-                    print("Lagu %s sudah berhasil dihapus" % (judul))
+                if data_lagu.hapus_lagu(judul):
+                    print(f"Lagu {judul} berhasil dihapus")
                     print("")
                 
                 else:
-                    print("Lagu %s tidak ditemukan" % (judul))
+                    print(f"Lagu {judul} tidak ditemukan")
                     print("")
         
         elif opsi == 3:
-            if len(list_lagu) == 0:
+            if data_lagu.apakah_lagu_kosong():
                 print("Belum ada lagu untuk saat ini")
                 print("")
             
@@ -139,23 +94,13 @@ if __name__ == "__main__":
                 judul = input("> ").strip()
                 print("")
 
-                index_lagu = dapatkan_index_lagu(judul)
+                lagu = data_lagu.dapatkan_lagu(judul)
 
-                if index_lagu == -1:
-                    print("Lagu %s tidak ditemukan" % (judul))
+                if lagu is None:
+                    print(f"Lagu {judul} tidak ditemukan")
                     print("")
 
                 else:
-                    def dapatkan_input_dan_cek(pesan, pesan_salah, regex):
-                        while True:
-                            print(pesan)
-                            string_input = input("> ").strip()
-
-                            if re.search(regex, string_input) is not None:
-                                return string_input
-                            
-                            print(pesan_salah)
-                    
                     judul = dapatkan_input_dan_cek("Masukkan judul lagu (Kosongkan bila tidak ingin diubah)", "Input judul tidak sesuai, harap diulang!", r"^$|^\w[\w\s']*$")
                     album = dapatkan_input_dan_cek("Masukkan Album lagu (Kosongkan bila tidak ingin diubah)", "Input album tidak sesuai, harap diulang!", r"^$|^\w[\w\s']*$")
                     penyanyi = dapatkan_input_dan_cek("Masukkan penyayi lagu (Kosongkan bila tidak ingin diubah)", "Input penyanyi tidak sesuai, harap diulang!", r"^$|^\w[\w\s']*$")
@@ -164,20 +109,20 @@ if __name__ == "__main__":
                     durasi = dapatkan_input_dan_cek("Masukkan durasi lagu [mm:dd] (Kosongkan bila tidak ingin diubah)", "Input durasi tidak sesuai, harap diulang!", r"^$|^[0-9]{2}:[0-9]{2}$")
                     print("")
 
-                    data_edit = edit_lagu(list_lagu[index_lagu], judul, penyanyi, album, genre, tahun, durasi)
+                    list_properti_yang_diedit = data_lagu.edit_lagu(lagu, judul, penyanyi, album, genre, tahun, durasi)
 
-                    if len(data_edit) == 0:
-                        print("Data lagu %s tidak ada yang diubah" % (judul))
+                    if list_properti_yang_diedit is None:
+                        print(f"Data lagu {lagu["judul"]} tidak ada yang diubah")
                         print("")
 
                     else:
-                        for data in data_edit:
-                            print("Data %s lagu berhasil diubah %s -> %s" % (data["tipe"], data["data_sebelumnya"], data["data_baru"]))
+                        for properti_yang_diedit in list_properti_yang_diedit:
+                            print(f"{properti_yang_diedit["tipe"].capitalize()} lagu berhasil diubah, {properti_yang_diedit["data_sebelumnya"]} -> {properti_yang_diedit["data_baru"]}")
                         
                         print("")
         
         elif opsi == 4:
-            if len(list_lagu) == 0:
+            if data_lagu.apakah_lagu_kosong():
                 print("Belum ada lagu untuk saat ini")
                 print("")
             
@@ -185,20 +130,11 @@ if __name__ == "__main__":
                 print("Daftar Lagu:")
                 print("------------")
 
-                lagu_terurut = []
-                lagu_terurut.extend(list_lagu)
-                lagu_terurut.sort(key = lambda elem: elem["judul"])
-
-                urutan_ke = 1
-
-                for lagu in list_lagu:
-                    print("%i. %s, %s (%s)" % (urutan_ke, lagu["judul"], lagu["penyanyi"], lagu["durasi"]))
-                    urutan_ke += 1
-
+                data_lagu.print_semua_lagu("judul")
                 print("")
         
         elif opsi == 5:
-            if len(list_lagu) == 0:
+            if data_lagu.apakah_lagu_kosong():
                 print("Belum ada lagu untuk saat ini")
                 print("")
             
@@ -207,28 +143,23 @@ if __name__ == "__main__":
                 judul = input("> ").strip()
                 print("")
 
-                index_lagu = dapatkan_index_lagu(judul)
+                lagu = data_lagu.dapatkan_lagu(judul)
 
-                if index_lagu == -1:
-                    print("Lagu %s tidak ditemukan" % (judul))
+                if lagu is None:
+                    print(f"Lagu {judul} tidak ditemukan")
                     print("")
                 
                 else:
-                    lagu = list_lagu[index_lagu]
+                    print(f"Detail Lagu {judul}:")
+                    print("-------------" + '-' * len(judul))
 
-                    print("Detail Lagu '%s':" % (lagu["judul"]))
-                    print("---------------" + '-' * len(lagu["judul"]))
-                    print("Judul : %s" % (lagu["judul"]))
-                    print("Penyanyi : %s" % (lagu["penyanyi"]))
-                    print("Album : %s" % (lagu["album"]))
-                    print("Genre : %s" % (lagu["genre"]))
-                    print("Tahun : %s" % (lagu["tahun"]))
-                    print("Durasi : %s" % (lagu["durasi"]))
+                    data_lagu.print_detail_lagu(lagu)
                     print("")
         
         elif opsi == 6:
+            data_lagu.keluar()
             print("Anda keluar dari program!")
             print("")
             break
 
-        input("Tekan enter untuk melanjutkan")
+        input("Tekan enter untuk melanjutkan ")
